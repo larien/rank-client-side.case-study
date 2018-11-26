5223fecec0ba33bb677afbd2bebd856b6a2bcf2b
Lauren Ferreira <lauren.@gmail.com>
2018-11-26 09:48:43 -0200

    App implemented in index.js

--- rank/src/index.js
+++ rank/src/index.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { store } from './redux/store';
+import { store, history } from './redux/store';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 ReactDOM.render((
     <Provider store={store}>
-        <App />
+        <ConnectedRouter history={ history }>
+            <Switch>
+                <Route path="/reviews" component={App}/>
+            </Switch>
+        </ConnectedRouter>
     </Provider>
 ), document.getElementById('root'));
 
