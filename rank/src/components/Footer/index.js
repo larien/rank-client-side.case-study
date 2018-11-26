583669a56dd6d9cbfd952bbcbb6e59a96b3aac33
Lauren Ferreira <lauren.@gmail.com>
2018-11-26 13:14:48 -0200

    Footer component created

---
+++ rank/src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './style.css';
+
+export default class Footer extends Component {
+  render() {
+    return (
+        <div className="footer"><p>© 2018 Lauren Ferreira</p></div>
+    )
+  }
+}
