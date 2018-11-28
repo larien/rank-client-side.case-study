0f375d7e3695ffb49e07050e2e298c00eec7aca9
Lauren Ferreira <lauren.@gmail.com>
2018-11-28 14:05:29 -0200

    Component implemented ir App.js

--- rank/src/App.js
+++ rank/src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import './App.css';
-import Review from './components/scenes/Reviews/Review';
-import Reviews from './components/scenes/Reviews/Reviews';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import './App.css'
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header'
+
+import Review from './components/scenes/Reviews/Review'
+import Reviews from './components/scenes/Reviews/Reviews'
+import Editor from './components/scenes/Reviews/Editor'
+
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
@@ -17,13 +20,14 @@ class App extends Component {
           <Switch>
             <Route exact path="/reviews" component={Reviews} />
             <Route path="/reviews/:id" component={Review} />
+            <Route path="/editor" component={Editor} />
             <Route path="**" component={Reviews} />
           </Switch>
           </div>
           <Footer className="Footer"/>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
