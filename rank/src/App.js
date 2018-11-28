5f9d5e09d71763fb9cb9118281d0fb8ccfff6f73
Lauren Ferreira <lauren.@gmail.com>
2018-11-28 12:08:51 -0200

    Implementing Review component in App.js

--- rank/src/App.js
+++ rank/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Review from './components/scenes/Reviews/Review';
+import Reviews from './components/scenes/Reviews/Reviews';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
+      
       <div className="Site">
         <Header className="Header"/>
-        <p className="Content">Hello, Rank!</p>
-        <Footer className="Footer"/>
+        <div className="Content">
+          <Switch>
+            <Route exact path="/reviews" component={Reviews} />
+            <Route path="/reviews/:id" component={Review} />
+            <Route path="**" component={Reviews} />
+          </Switch>
+          </div>
+          <Footer className="Footer"/>
       </div>
     );
   }
