7d0503169cbdd1b75c85549be58f249ff0d0c9af
Lauren Ferreira <lauren.@gmail.com>
2018-12-02 13:09:49 -0200

    Implementing categories reducer in reducers file

--- rank/src/redux/reducer.js
+++ rank/src/redux/reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import reviews from './reducers/reviews'
 import games from './reducers/games'
 import common from './reducers/common'
+import categories from './reducers/categories'
 import { routerReducer } from 'react-router-redux'
 
 export default combineReducers({
     reviews,
     games,
+    categories,
     common,
     router: routerReducer
 })
