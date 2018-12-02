9c723cd78e811ec76c6f92e324bf0727ef3a9cd1
Lauren Ferreira <lauren.@gmail.com>
2018-12-02 13:18:06 -0200

    Categories implemented in Games page

--- rank/src/components/scenes/Games/Games/index.js
+++ rank/src/components/scenes/Games/Games/index.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react'
 import { getGames } from '../../../../redux/actions/game'
+import { getCategories } from '../../../../redux/actions/categories'
 import { connect } from 'react-redux'
 import './style.css'
 
 const mapStateToProps = state => {
     return ({
-        games: state.games.games
+        games: state.games.games,
+        categories: state.categories.categories
     })
 }
 
 const mapDispatchToProps = {
-    getGames
+    getGames,
+    getCategories
 }
 
 export class Games extends Component {
@@ -18,6 +21,7 @@ export class Games extends Component {
       super(props)
 
       this.props.getGames()
+      this.props.getCategories()
    }
 
   render() {
@@ -27,15 +31,27 @@ export class Games extends Component {
           <div className="data">
             <h3 className="title"><a href={`/games/${game.id}`}>{game.name}</a></h3>
             <div className="body">
-                <p className="categories">Categories: { game.categories }</p>
+                <p>Categories: { game.categories }</p>
                 <p className="platforms">Platforms: { game.platforms }</p>
                 <p className="score">Score: { game.score }</p>
             </div>
           </div>
         </div>
       )
+
+      const categories = this.props.categories.map((category)=>
+      <div className="category">
+          <div className="data">
+            <button className="title"><a href={`/games/categories/${category}`}>{category}</a></button>
+          </div>
+        </div>
+      )
+
     return (
     <div>
+      <div className="categories">
+        { categories }
+      </div>
         <div className={games.length === 0 ? '' : 'hidden'}><p className="nogames">There are no games here!</p>
         <p className="nogames">Why don't you try to create a new one? Up there!</p></div>
       <div className="games">
