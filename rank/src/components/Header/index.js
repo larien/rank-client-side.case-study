8d4e776f3b6963b5525400b520b3190e2f2b4352
Lauren Ferreira <lauren.@gmail.com>
2018-12-04 14:33:28 -0200

    Implementing Conditional Rendering in NavBar

--- rank/src/components/Header/index.js
+++ rank/src/components/Header/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './style.css';
 import logo from './logo.svg';
-import auth0Client from '../../auth/Auth';
 
 export default class Header extends Component {
+  
   render() {
+    const isLoggedIn = () => {
+      return localStorage.getItem("token") !== null
+    }
     const signOut = () => {
-      auth0Client.signOut();
-      this.props.history.replace('/');
+      localStorage.removeItem("token")
+      window.location.reload()
     };
     return (
         <header className="header">
@@ -17,17 +20,18 @@ export default class Header extends Component {
           </h3>
           <ul className="list">
             <li className="item"><a className="link" href="/reviews">Reviews</a></li>
-            <li className="item"><a className="link" href="/editor/review">New Review</a></li>
+            {isLoggedIn() && 
+              <li className="item"><a className="link" href="/editor/review">New Review</a></li>
+            }
             <li className="item"><a className="link" href="/games">Games</a></li>
-            <li className="item"><a className="link" href="/editor/game">New Game</a></li>
-            {
-              !auth0Client.isAuthenticated() &&
-              <li className="item"><a className="link" onClick={auth0Client.signIn}>Sign In</a></li>
+            {isLoggedIn() && 
+              <li className="item"><a className="link" href="/editor/game">New Game</a></li>
             }
-            {
-              auth0Client.isAuthenticated() &&
+            {!isLoggedIn() ? (
+              <li className="item"><a className="link" href="/auth">Sign In</a></li>
+            ) : (
               <li className="item"><a className="link" onClick={() => {signOut()}}>Sign Out</a></li>
-            }
+            )}
             
           </ul>
         </nav>
