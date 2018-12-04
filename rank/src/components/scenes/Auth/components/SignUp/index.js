e4a3c64d37265759a534f10031fc7177953c680d
Lauren Ferreira <lauren.@gmail.com>
2018-12-04 14:34:19 -0200

    Implemented Sign Up logic

--- rank/src/components/scenes/Auth/components/SignUp/index.js
+++ rank/src/components/scenes/Auth/components/SignUp/index.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
 
 class SignUp extends Component {
+    constructor(){
+        super()
+        this.state = {
+            username: "",
+            password: ""
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange = (e) => {
+        e.preventDefault();
+        this.setState({ [e.target.name]: e.target.value})
+    }
+
+    signUp = async e => {
+        e.preventDefault();
+        const _url = process.env.NODE_ENV === 'production' ? "/api/v1/" : "http://localhost:8899/api/v1/"
+
+        const response = await fetch(`${_url}signup`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              username: this.state.username,
+              password: this.state.password
+            }),
+        });
+        const body = await response.text();
+        console.log(body);
+      };
+
   render() {
     return (
       <div>
-        <form className="login-form">
+        <form onClick={this.signUp} onChange={this.handleChange} className="login-form">
             <h1>SIGN UP</h1>
             <div className="fields">
                 <p>
-                    <label htmlFor="email">USERNAME</label>
-                    <input type="text" name="email" id="email"/>
+                    <label htmlFor="username">USERNAME</label>
+                    <input type="text" name="username" id="username"/>
                 </p>
                 <p>
                     <label htmlFor="password">PASSWORD</label>
