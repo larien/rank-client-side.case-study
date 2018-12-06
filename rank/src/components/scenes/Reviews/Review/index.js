32541be091836448c7f342ae06a51dbbe0286564
Lauren Ferreira <lauren.@gmail.com>
2018-12-05 22:48:46 -0200

    Implementing Review style and Rating info

--- rank/src/components/scenes/Reviews/Review/index.js
+++ rank/src/components/scenes/Reviews/Review/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getReview } from '../../../../redux/actions/review';
+import './Review.css';
 
 const mapStateToProps = state => {
     return {
@@ -23,25 +24,38 @@ class Review extends Component {
     }
 
     render() {
-        const { title, cover_image, author_nickname, average_rating, publication_date, reading_time, text_review } = this.props._review
-
+        const { title, cover_image, author_nickname, average_rating, updated_at, reading_time, text_review } = this.props._review
         return (
-        <div className="review">
-            <div className="thumbnail">
-            <h3 className="info">{title}</h3>
-            <img src={cover_image} alt="Thumb" />
+            <div className="review">
+                <div className="reviewthumbnail">
+                    <h3 className="info">{title}</h3>
+                    <img src={cover_image} alt="Thumb" />
+                </div>
+                <div className="reviewinfo">
+                    <p>Author: {author_nickname}</p>
+                    <p>Publication Date: {updated_at}</p>
+                    <p>Reading Time: {reading_time}</p>
+                    <p>Review: {text_review}</p>
+                </div>
+                <div className="reviewrate">
+                    <p>Average Rating: {average_rating}</p>
+                    <form>
+                        <div className="field info-rating">
+                                    <div>
+                                        <h3 htmlFor="rating">Rating</h3> 
+                                    </div>
+                                    <div>
+                                        <input type="number"/>
+                                    </div>
+                        </div>
+                        <div className="submitbutton">
+                                <button type="submit">RATE GAME</button>
+                        </div>
+                    </form>
+                    
+                </div>
             </div>
-            <div>
-                <p>Author: {author_nickname}</p>
-                <p>Average Rating: {average_rating}</p>
-                <p>Publication Date: {publication_date}</p>
-                <p>Reading Time: {reading_time}</p>
-            </div>
-            <div className="text">
-                <p>Review: {text_review}</p>
-            </div>
-        </div>
-        )
+            )
     }
 }
 export default connect(mapStateToProps, {
